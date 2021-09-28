45d0d9dfc7c25398c275872cb26a2c21a66b2d63
Roger Marsh <roger.marsh@solentware.co.uk>
2021-09-28 16:52:07 +0000

    Adjust version to 0.7.3 in setup.py.
    Source distributions uploaded to solentware.co.uk.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     setup(
         name="emailextract",
-        version="0.7.3.dev0",
+        version="0.7.3",
         description="Extract text from emails",
         author="Roger Marsh",
         author_email="roger.marsh@solentware.co.uk",
@@ -39,8 +39,8 @@ if __name__ == "__main__":
             "Intended Audience :: Developers",
             "Development Status :: 3 - Alpha",
         ],
-        install_requires=["solentware-misc==1.3.1.dev0"],
+        install_requires=["solentware-misc==1.3.1"],
         dependency_links=[
-            "http://solentware.co.uk/files/solentware-misc-1.3.1.dev0.tar.gz",
+            "http://solentware.co.uk/files/solentware-misc-1.3.1.tar.gz",
         ],
     )
